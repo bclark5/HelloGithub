9204050dffce611c2899a00f9dd363a5bb349150
bclark5 <bclark5@zagmail.gonzaga.edu>
2020-01-16 21:51:39 -0800

    initial

---
+++ HelloGithub/src/Hello.java
@@ -0,0 +1,24 @@
+public class Hello {
+    public static void main(String Args[]){
+        System.out.println("hello");
+        System.out.println("Brandon");
+    }
+}
+//version control: a tool for tracking changes
+// helloworldgithub is a folder
+//to make this folder a repository(or repo for short)
+//I need to put it under version control
+// a commdit is a package of changes with a label
+//label<-->message
+//git init to initialize git
+//git add to make a commit
+//git status to see status of commit
+// git commit -m to make a message
+//git log to see each commit and things that occured
+
+
+//dir /a gives listing of all the contents of the current working
+// directory, including all hidden files
+
+//local repository. we want to push this repo up to the cloud.
+// to do this, we need a remote repository.
